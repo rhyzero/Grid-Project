ae3b8df4a556cacc7604129d7d58860cadaf25f5
Jacky Chen <jackyc4200@gmail.com>
2023-10-04 22:55:40 -0400

    added add column btn

--- script.js
+++ script.js
@@ -6,14 +6,46 @@ newCell.classList.add('cell')
 let gridContainer = document.querySelector('.grid-container')
 
 document.querySelector('.add-row-btn').addEventListener('click', function() {
-    if (colAmount === 0) {
+    if (colAmount == 0) {
         colAmount = colAmount + 1
+        cellCounter = cellCounter + 1
+        rowAmount = rowAmount + 1
+        gridContainer.appendChild(newCell.cloneNode(true))
     }
-    cellCounter = cellCounter + 1
-    rowAmount = rowAmount + 1
-    console.log("rowAmount = " + rowAmount);
-    console.log('colAmount = ' + colAmount);
-    console.log('cellCounter = ' + cellCounter);
-    gridContainer.appendChild(newCell.cloneNode(true))
+    else if (colAmount == 1) {
+        cellCounter = cellCounter + 1
+        rowAmount = rowAmount + 1
+        gridContainer.appendChild(newCell.cloneNode(true))
+    }
+    else if (colAmount >= 2) {
+        rowAmount = rowAmount + 1
+        cellCounter = cellCounter + colAmount
+        for (let i = 0; i < colAmount; i++){
+            gridContainer.appendChild(newCell.cloneNode(true))
+        }
+    }
 })
 
+document.querySelector('.add-col-btn').addEventListener('click', function() {
+    if (rowAmount == 0) {
+        rowAmount = rowAmount + 1
+        cellCounter = cellCounter + 1
+        colAmount = colAmount + 1
+        gridContainer.style.gridTemplateColumns = `repeat(${colAmount}, 1fr)`
+        gridContainer.appendChild(newCell.cloneNode(true))
+    }
+    else if (rowAmount == 1) {
+        cellCounter = cellCounter + 1
+        colAmount = colAmount + 1
+        gridContainer.style.gridTemplateColumns = `repeat(${colAmount}, 1fr)`
+        gridContainer.appendChild(newCell.cloneNode(true))
+    }
+    else {
+        colAmount = colAmount + 1
+        cellCounter = cellCounter + rowAmount
+        gridContainer.style.gridTemplateColumns = `repeat(${colAmount}, 1fr)`
+        for (let i = 0; i < rowAmount; i++){
+            gridContainer.appendChild(newCell.cloneNode(true))
+        }
+    }
+})
